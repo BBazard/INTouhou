56a9b7180ad3621b271308a8b5c22693a4e2a4f1
Benoit Bazard <benoit.bazard@telecom-sudparis.eu>
2016-06-11 14:03:32 +0200

    Add variables in PlayerMove::run for readability

--- src/playermove.cpp
+++ src/playermove.cpp
@@ -6,6 +6,8 @@ PlayerMove::PlayerMove() {
 
 void PlayerMove::run(World &world) {
   for (size_t i = 0; i < world.getBitset(EVENT).size(); ++i) {
+    int LIMIT_WIDTH = WINDOW_WIDTH - world.getSprites(i).getSizeX();
+    int LIMIT_HEIGHT = WINDOW_HEIGHT - world.getSprites(i).getSizeY();
     if (world.getBitset(EVENT)[i]) {
       if (world.getEvent(i).getMove()[LEFT] == true) {
         if (world.getPositions(i).x() - 5 < 0)
@@ -14,8 +16,8 @@ void PlayerMove::run(World &world) {
           world.getPositions(i).move(-5, 0);
       }
       if (world.getEvent(i).getMove()[RIGHT] == true) {
-        if (world.getPositions(i).x() + 5 > WINDOW_WIDTH - world.getSprites(i).getSizeX())
-          world.getPositions(i).setX(WINDOW_WIDTH - world.getSprites(i).getSizeX());
+        if (world.getPositions(i).x() + 5 > LIMIT_WIDTH)
+          world.getPositions(i).setX(LIMIT_WIDTH);
         else
           world.getPositions(i).move(5, 0);
       }
@@ -26,8 +28,8 @@ void PlayerMove::run(World &world) {
           world.getPositions(i).move(0, -5);
       }
       if (world.getEvent(i).getMove()[DOWN] == true) {
-        if (world.getPositions(i).y() + 5 > WINDOW_HEIGHT - world.getSprites(i).getSizeY())
-          world.getPositions(i).setY(WINDOW_HEIGHT - world.getSprites(i).getSizeY());
+        if (world.getPositions(i).y() + 5 > LIMIT_HEIGHT)
+          world.getPositions(i).setY(LIMIT_HEIGHT);
         else
           world.getPositions(i).move(0, 5);
       }
