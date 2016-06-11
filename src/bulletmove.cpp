b3bd262329056d55937a1fb2c483174c46628a20
Benoit Bazard <benoit.bazard@telecom-sudparis.eu>
2016-06-11 23:16:07 +0200

    Set the bitset to false when bullets leave view
    Currently the bitset vector increases for nothing.
    We need to delete or reuse the unused case.

--- src/bulletmove.cpp
+++ src/bulletmove.cpp
@@ -8,6 +8,18 @@ void BulletMove::run(World &world) {
   for (size_t i = 0; i < world.getBitset(TARGET).size(); ++i) {
     if (world.getBitset(DYNAMICS)[i]) {
       world.getDynamic(i).move(world.getPosition(i));
+
+      auto& s = world.getSprite(i);
+      auto& pos = world.getPosition(i);
+      
+      // if bullet outside view
+      if ((pos.y() + s.getSizeY() < 0) ||
+          (pos.y() > WINDOW_HEIGHT)    ||
+          (pos.x() + s.getSizeX() < 0) ||
+          (pos.x() > WINDOW_WIDTH)) {
+        for (int j = 0; j < COMPONENTNUMBER; ++j)
+          world.getBitset((ComponentType)j)[i] = false;
+      }
     }
   }
 }
