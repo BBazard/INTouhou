978090afc127cf31ab78d8f5f6758698d782e320
Guillaume Chabbert <guillaume.chabbert@telecom-sudparis.eu>
2016-06-14 18:19:27 +0000

    Correct some indentation problems

--- src/collide.cpp
+++ src/collide.cpp
@@ -9,30 +9,30 @@ Collide::Collide() {
  * @todo when a player die, end the game
  */
 void Collide::run(World &world) {
- for (size_t i = 0; i < world.getBitset(TARGET).size(); ++i) {
-   if (world.getBitset(TARGET)[i]) {
-     if (world.getTarget(i).getTarget() == ENEMY) {
-       for (size_t j = 0; j < world.getBitset(HITBOX).size(); ++j) {
-         if (world.getEntityType(j) == ENEMYSHIP) {
-           auto posBullet = world.getPosition(i);
-           auto posShip = world.getPosition(j);
+  for (size_t i = 0; i < world.getBitset(TARGET).size(); ++i) {
+    if (world.getBitset(TARGET)[i]) {
+      if (world.getTarget(i).getTarget() == ENEMY) {
+        for (size_t j = 0; j < world.getBitset(HITBOX).size(); ++j) {
+          if (world.getEntityType(j) == ENEMYSHIP) {
+            auto posBullet = world.getPosition(i);
+            auto posShip = world.getPosition(j);
 
-           auto hBullet = world.getHitbox(i);
-           auto hEnemy = world.getHitbox(j);
+            auto hBullet = world.getHitbox(i);
+            auto hEnemy = world.getHitbox(j);
 
-           hBullet.setPosition(posBullet);
-           hEnemy.setPosition(posShip);
+            hBullet.setPosition(posBullet);
+            hEnemy.setPosition(posShip);
 
-           sf::FloatRect r1(hEnemy.x(), hEnemy.y(), hEnemy.w(), hEnemy.h());
-           sf::FloatRect r2(hBullet.x(), hBullet.y(), hBullet.w(), hBullet.h());
+            sf::FloatRect r1(hEnemy.x(), hEnemy.y(), hEnemy.w(), hEnemy.h());
+            sf::FloatRect r2(hBullet.x(), hBullet.y(), hBullet.w(), hBullet.h());
 
-           if (r1.intersects(r2)) {
-             world.removeEntity(i);
-             world.getLife(j).decreaseLife();
-             world.modifyScore(500);
+            if (r1.intersects(r2)) {
+              world.removeEntity(i);
+              world.getLife(j).decreaseLife();
+              world.modifyScore(500);
 
-             if (world.getLife(j).getLife() < 1)
-              world.removeEntity(j);
+              if (world.getLife(j).getLife() < 1)
+               world.removeEntity(j);
             }
           }
         }
